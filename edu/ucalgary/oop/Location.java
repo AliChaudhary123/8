f062a5394702ab5e54072e0782fb4b480171d8b7
Ali Chaudhary <alichaudhary2073@gmail.com>
2025-02-20 16:44:02 -0700

    First

---
+++ edu/ucalgary/oop/Location.java
@@ -0,0 +1,15 @@
+package edu.ucalgary.oop;
+
+import java.util.ArrayList;
+
+public class Location{
+    private String name;
+    private String address;
+    private ArrayList<DisasterVictim> occupants;
+    private ArrayList<Supply> supplies;
+
+    public Location(String name, String address)
+    {
+        
+    }
+}
