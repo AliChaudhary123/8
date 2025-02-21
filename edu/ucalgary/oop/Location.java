e9af84fc0c60e6a47bd9280176d3e2d43274c8e3
Ali Chaudhary <alichaudhary2073@gmail.com>
2025-02-20 17:20:33 -0700

    location and supply

--- edu/ucalgary/oop/Location.java
+++ edu/ucalgary/oop/Location.java
@@ -10,6 +10,58 @@ public class Location{
 
     public Location(String name, String address)
     {
-        
+        this.name = name;
+        this.address = address;
+        this.occupants = new ArrayList<DisasterVictim>();
+        this.supplies = new ArrayList<Supply>();
+    }
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+    public void setAddress(String address)
+    {
+        this.address = address;
+    }
+    public void setOccupants(ArrayList<DisasterVictim> occupants)
+    {
+        this.occupants = occupants;
+    }
+    public void setSupplies(ArrayList<Supply> supplies)
+    {
+        this.supplies = supplies;
+    }
+    public void addOccupant(DisasterVictim occupant)
+    {
+        this.occupants.add(occupant);
+    }
+    public void removeOccupant(DisasterVictim occupant)
+    {
+        this.occupants.remove(occupant);
+
+    }
+    public void addSupply(Supply supply){
+        this.supplies.add(supply);
+    }
+
+    public void removeSupply(Supply supply)
+    {
+        this.supplies.remove(supply);
+    }
+    public String getName()
+    {
+        return this.name;
+    }
+    public String getAddress()
+    {
+        return this.address;
+    }
+    public ArrayList<DisasterVictim> getOccupants()
+    {
+        return this.occupants;
+    }
+    public ArrayList<Supply> getSupplies(){
+        return this.supplies;
     }
 }
