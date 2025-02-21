2101cec15c04cd64e1e2abfcee5ef5850f6579f3
Ali Chaudhary <alichaudhary2073@gmail.com>
2025-02-20 17:54:14 -0700

    inquirer added

---
+++ edu/ucalgary/oop/Inquirer.java
@@ -0,0 +1,37 @@
+package edu.ucalgary.oop;
+
+public class Inquirer {
+    private String firstName;
+    private String lastName;
+    private String info;
+    private String servicesPhoneNum;
+
+    public Inquirer(String firstName, String lastName, String servicesPhoneNum, String info){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.servicesPhoneNum = servicesPhoneNum;
+        this.info = info;
+    }
+
+    public String getFirstName()
+    {
+        return firstName;
+    }
+
+    public String getLastName()
+    {
+        return lastName;
+    }
+
+    public String getServicesPhoneNum()
+    {
+        return servicesPhoneNum;
+    }
+
+    public String info()
+    {
+        return info;
+    }
+
+    
+}
